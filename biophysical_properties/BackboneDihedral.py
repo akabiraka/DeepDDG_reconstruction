9c6bc8c84047099b80d4d062cc688405e24b0650
AK <akabir0101@gmail.com>
2021-10-21 22:14:55 -0400

    subset of residues' angles can be selected, which can also give angles for all residues

--- biophysical_properties/BackboneDihedral.py
+++ biophysical_properties/BackboneDihedral.py
@@ -2,6 +2,7 @@ import sys
 sys.path.append("../DeepDDG_reconstruction")
 
 import math
+import numpy as np
 from Bio.PDB import PDBParser
 from Bio.PDB.vectors import calc_dihedral
 
@@ -9,8 +10,9 @@ class BackboneDihedral(object):
     def __init__(self) -> None:
         super().__init__()
         
-    def get_angles(self, pdb_file, residue_num, chain_id="A"):
+    def get_angles_of_a_residue(self, pdb_file, residue_num, chain_id="A", type=None):
         """
+        Returns phi, psi and omega angles of a residue.
         # used the following two links to compute phi, psi and omega angels
             # https://biopython.org/docs/dev/api/Bio.PDB.internal_coords.html#Bio.PDB.internal_coords.IC_Residue.pick_angle
             # https://biopython.org/docs/latest/api/Bio.PDB.vectors.html?highlight=calc_dihedral#Bio.PDB.vectors.calc_dihedral
@@ -22,8 +24,11 @@ class BackboneDihedral(object):
             pdb_file ([type]): [description]
             residue_num ([type]): [description]
             chain_id (str, optional): [description]. Defaults to "A".
+            type (str, optional): How the angles will be returned. 
+                Type can be "both", "sin", "cos" or None. 
+                The default is phi, psi and omega values in a array.
         Returns:
-            phi, psi and omega 
+            1D np array: angles.
         """
         residue = PDBParser(QUIET=True).get_structure("", pdb_file)[0][chain_id][residue_num]
         next_residue = PDBParser(QUIET=True).get_structure("", pdb_file)[0]["A"][residue_num+1]
@@ -34,30 +39,44 @@ class BackboneDihedral(object):
         psi = calc_dihedral(sN, sCA, sC, nN)
         omega = calc_dihedral(sCA, sC, nN, nCA)
         phi = calc_dihedral(sC, nN, nCA, nC)
-        return phi, psi, omega
+        
+        if type=="sin": return np.array([math.sin(phi), math.sin(psi), math.sin(omega)])
+        elif type=="cos": return np.array([math.cos(phi), math.cos(psi), math.cos(omega)])
+        elif type=="both": return np.array([math.sin(phi), math.sin(psi), math.sin(omega), math.cos(phi), math.cos(psi), math.cos(omega)])
+        else: return np.array([phi, psi, omega])
     
-    def get_sine_cos_of_angels(self, pdb_file, residue_num, chain_id="A"):
-        """Computes sin and cos of backbone dihedral angles: phi, psi and omega
+    
+    def get_angles_of_residues(self, pdb_file, chain_id="A", from_residue=1, n_residues=None, type=None):
+        """It conputes angles for a subset of residues using from_residue and n_residues parameter.
+        If they are not passed, the angles of all residues of the protein will be returned.
 
         Args:
-            pdb_file (str): file path
-            residue_num (int): [description]
-            chain_id (str, optional): [description]. Defaults to "A".
+            pdb_file (str): a pdb file path
+            chain_id (str, optional): chain id. Defaults to "A".
+            from_residue (int, optional): a residue number. Defaults to 1.
+            n_residues (int, optional): how many residues to consider. Defaults to None.
+            type (str, optional): how the angles will be returned. 
+                How the angles will be returned. 
+                Type can be "both", "sin", "cos" or None. 
 
         Returns:
-            float: sin and cos of phi, psi and omega
+            2D np array: angles.
         """
-        phi, psi, omega = self.get_angles(pdb_file=pdb_file, residue_num=residue_num, chain_id=chain_id)
+        chain = PDBParser(QUIET=True).get_structure("", pdb_file)[0][chain_id]
+        if n_residues is None: n_residues = len(list(chain.get_residues()))
+        else: n_residues = from_residue+n_residues 
+        all_angles = []
+        for i in range(from_residue, n_residues):
+            all_angles.append(self.get_angles_of_a_residue(pdb_file, i, chain_id, type))
         
-        sin_phi, sin_psi, sin_omega = math.sin(phi), math.sin(psi), math.sin(omega)
-        cos_phi, cos_psi, cos_omega = math.cos(phi), math.cos(psi), math.cos(omega)
-        return sin_phi, sin_psi, sin_omega, cos_phi, cos_psi, cos_omega
+        return np.array(all_angles)
+            
+        
+pdb_file = "data/pdbs_clean/1a5eA.pdb"        
+bd = BackboneDihedral()
 
-# pdb_file = "data/pdbs_clean/1a5eA.pdb"        
-# bd = BackboneDihedral()
-# phi, psi, omega = bd.get_angles(pdb_file=pdb_file, residue_num=121, chain_id="A")
-# print(phi, psi, omega)
+angles = bd.get_angles_of_a_residue(pdb_file=pdb_file, residue_num=121, chain_id="A", type="both")
+print(angles)
 
-# sin_phi, sin_psi, sin_omega, cos_phi, cos_psi, cos_omega = bd.get_sine_cos_of_angels(pdb_file=pdb_file, residue_num=121, chain_id="A")
-# print(sin_phi, sin_psi, sin_omega, cos_phi, cos_psi, cos_omega)
-
+angles = bd.get_angles_of_residues(pdb_file=pdb_file, chain_id="A", from_residue=150, n_residues=6, type="both")
+print(angles)
